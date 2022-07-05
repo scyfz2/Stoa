170b58339aa9fb93ec104cf3262e2ebf4feaa150
BoyuanYE <scyby1@nottingham.edu.cn>
2022-07-05 17:23:02 +0800

    feat: :sparkles: 新增迎新周日程板块后端业务模型以及接口。
    目前所有基础功能已实现，全部通过swagger测试。

--- nuoquan-dev/nuoquan-dev-api/src/main/java/com/nuoquan/controller/BasicController.java
+++ nuoquan-dev/nuoquan-dev-api/src/main/java/com/nuoquan/controller/BasicController.java
@@ -49,6 +49,8 @@ public class BasicController {
 	public NotifyRemindService notifyRemindService;
 	@Autowired
 	public AdvertService advertService;
+	@Autowired
+	public EventsCalendarService eventsCalendarService;
 	
 	// 每页分页的记录数
 	public static final Integer PAGE_SIZE = 10;
