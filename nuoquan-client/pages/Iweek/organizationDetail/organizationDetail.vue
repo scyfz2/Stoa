478b6e3a94b4cb62981b362a30a809a17badede1
huangzhuoli <scyzh7@nottingham.edu.cn>
2022-09-07 23:24:08 +0800

    add longpresss

--- nuoquan-client/pages/Iweek/organizationDetail/organizationDetail.vue
+++ nuoquan-client/pages/Iweek/organizationDetail/organizationDetail.vue
@@ -46,19 +46,21 @@
  				</view>
  			</view>
  			<!-- 招新推文以及介绍的二维码 -->
- 			<view class="orgQRBox" v-if="detail.imgList">
- 				<view v-if="detail.imgList[0]">
- 					<image :src="pathFilter(detail.imgList[0].imagePath)" mode="aspectFill" class="orgQR"></image>
- 				</view>
- 				<view v-if="detail.imgList[1]">
- 					<image :src="pathFilter(detail.imgList[1].imagePath)" mode="aspectFill" class="orgQR"></image>
- 				</view>
- 			</view>
- 			<view class="orgQRBox" v-if="detail.imgList">
- 				<view v-if="detail.imgList[2]">
- 					<image :src="pathFilter(detail.imgList[2].imagePath)" mode="aspectFill" class="orgQR"></image>
- 				</view>
- 			</view>
+			<view  v-if="detail.imgList">
+				<view class="orgQRBox">
+					<view v-if="detail.imgList[0]">
+						<image :src="pathFilter(detail.imgList[0].imagePath)" mode="aspectFit" class="orgQR" @tap="previewImg(0)" @longpress="aboutImg(0)"></image>
+					</view>
+					<view v-if="detail.imgList[1]">
+						<image :src="pathFilter(detail.imgList[1].imagePath)" mode="aspectFit" class="orgQR" @tap="previewImg(1)" @longpress="aboutImg(1)"></image>
+					</view>
+				</view>
+				<view class="orgQRBox">
+					<view v-if="detail.imgList[2]">
+						<image :src="pathFilter(detail.imgList[2].imagePath)" mode="aspectFit" class="orgQR" @tap="previewImg(2)" @longpress="aboutImg(2)"></image>
+					</view>
+				</view>
+			</view>
  		</view>
  	</view>
  </template>
@@ -92,7 +94,61 @@
  			this.detail = temp;
  		},
  		methods: {
- 			
+ 			previewImg: function(index) {
+				var imgList = this.detail.imgList;
+				var arr = [];
+				var path;
+				for (var i = 0; i < imgList.length; i++) {
+					// console.log(imgList[i].imagePath);
+					path = this.pathFilter(imgList[i].imagePath);
+					arr = arr.concat(path);
+				}
+				// console.log(arr);
+			
+				uni.previewImage({
+					current: index,
+					urls: arr
+				});
+		},
+		aboutImg: function(index) {
+			var that = this;
+			console.log(this.detail.imgList[index].imagePath);
+			uni.showActionSheet({
+				itemList: ['保存图片到本地'],
+				success: function(res) {
+					console.log(res.tapIndex);
+					// 保存图片至本地
+					if (res.tapIndex == 0) {
+						uni.showLoading({
+							title: '下载中...'
+						});
+						uni.downloadFile({
+							url: that.pathFilter(that.detail.imgList[index].imagePath),
+							success: function(res) {
+								if (res.statusCode == 200) {
+									uni.saveImageToPhotosAlbum({
+										filePath: res.tempFilePath,
+										success: function() {
+											console.log('save success');
+											uni.hideLoading();
+										},
+										fail: function() {
+											console.log('save failed');
+											uni.hideLoading();
+											uni.showToast({
+												title: '保存失败',
+												icon: 'none',
+												duration: 1000
+											});
+										}
+									});
+								}
+							}
+						});
+					}
+				}
+			});
+		},
  		}
  	}
  </script>
